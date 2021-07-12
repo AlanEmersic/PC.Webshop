1f06bd8b8c892ba4775a691d2fb44080ab2f6789
Alan <alan.emersic@gmail.com>
2021-07-12 19:08:34 +0200

    order

--- PC.Webshop/Controllers/OrderController.cs
+++ PC.Webshop/Controllers/OrderController.cs
@@ -5,9 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using PC.Webshop.DAL;
 using PC.Webshop.Model;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace PC.Webshop.Web.Controllers
 {
